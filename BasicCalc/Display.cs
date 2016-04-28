5fdeab7dc2c543a85057f4efa9b237a6d191cf7e
VRStrickland <remmickstrickland@gmail.com>
2016-04-28 14:08:10 -0500

    Added files via upload

---
+++ BasicCalc/Display.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalc
+{
+    class CalcDisplay
+    {
+        public string UpdateDisplay(string InputString, string NewAddition)
+        {
+            string NewString = "";
+            if (!IsValidInput(InputString, NewAddition))
+            {
+                return InputString;
+            }
+            NewString = InputString + NewAddition;
+            return NewString;
+        }
+
+
+
+
+
+        private bool IsValidInput(string InputString, string NewAddition)
+        {
+            bool RetBool = true;
+
+            switch (NewAddition)
+            {
+                case ".":
+                    int res1 = InputString.IndexOf(".");
+                    if (res1 != -1)
+                        {
+                            RetBool = false;
+                        }
+                    break;
+                case "1":
+                default:
+                    RetBool = true;
+                    break;
+            }
+            return RetBool;
+
+
+
+
+        }
+
+    }
+}
